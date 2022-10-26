b61fd8f97e16f8c952ad52bdef2de7e93a44bc0a
German Viescas <germanviescas@gmail.com>
2022-10-26 18:13:00 -0300

    Serve index.html at "/"

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 )
 
@@ -35,6 +36,7 @@ func (s *Server) Listen() {
 	s.registerHealthCheckHandler()
 	s.registerAnalyticsHandler()
 	s.registerStaticHandler()
+	s.registerIndexHandler()
 
 	log.Printf("Server up on :%d\n", s.port)
 	err := s.server.ListenAndServe()
@@ -47,6 +49,13 @@ func (s *Server) registerStaticHandler() {
 	fs := http.FileServer(http.Dir("./pages"))
 	fsWithTimeout := http.TimeoutHandler(fs, 5*time.Second, "Timeout\n")
 	s.mux.Handle("/blog/", http.StripPrefix("/blog/", logger(fsWithTimeout)))
+}
+
+func (s *Server) registerIndexHandler() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join("pages", "index.html"))
+	}
+	s.mux.Handle("/", logger(http.HandlerFunc(handler)))
 }
 
 func (s *Server) registerHealthCheckHandler() {
